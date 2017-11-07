bba879f63ef99ad43e518731202e86155993d0e6
mickequeen <mickequeen@gmail.com>
2017-11-07 10:49:22 -0300

    twitter funcional v 0.0.3

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,19 +1,73 @@
-function add(){
 
-	autosize(document.querySelector('textarea'));
+function add(){
 	/*tomar texto ingresado en textarea*/
-	var comment= document.getElementById('comment');
+	var comments= document.getElementById('comment').value;
 
 	/*limpiar textarea*/
 	document.getElementById('comment').value='';
 
 	/*guardar contenedor html en variable*/
-	var cont= document.getElementByClassName('cont');
+	var cont= document.getElementById('cont');
 
-	/*validar que no esté vacío*/
-	if (comments.length==0 ||comments.length==null) {
-	alert("Debes escribir un mensaje");
-	return false;
-	}
-}
+  /*crear div para cada comentario*/
+	var newComments= document.createElement('div');
 
+  if (comments.length==0 || comments==null) {
+  alert("debes escribir un tweet");
+  return false;
+  }
+
+	var textNewComment = document.createTextNode(comments);
+
+	/*crear elemento parrafo para contener el texto ingresado*/
+	var contenedorComentario = document.createElement('p');
+
+	/*llenar contenedor para nuevo comentario*/
+	contenedorComentario.appendChild(textNewComment);
+	newComments.appendChild(contenedorComentario);
+	cont.appendChild(newComments)
+};
+
+/*contador  de caracteres, colorear contador*/
+(function charCont(){
+	/*encerrar en variable cada carácter ingresado en el text area*/
+  var msg = document.getElementsByClassName("txt")[0];
+      /*crear variable para contador de carácteres vacío en html*/
+      charLeft = document.getElementsByClassName('contador')[0];
+      /*establece el máximo de carácteres*/
+      maxChar = 140;
+   
+  	  /*mostrar en html carácteres iniciales*/
+  	  charLeft.innerHTML = maxChar;
+
+  	  // actualizar mientras se tipea
+  	  msg.onkeydown = function(){
+
+        setTimeout(function(){
+        	//actualizar cantidad de carácteres, máximo - largo de lo escrito
+          charLeft.innerHTML = maxChar - msg.value.length;
+          	//variable para colorear carácteres
+          var characters= msg.value.length;
+          	//coloreado según cantidad de carácteres
+          if (characters>=120 && characters<130) {
+          	charLeft.className+= ' purple';
+          }else if (characters>=130) {
+          	charLeft.className= charLeft.className.replace( /(?:^|\s)purple(?!\S)/g , '' )
+          	charLeft.className+= ' red'
+          }else{
+          	charLeft.className= ' contador';
+          }
+        }, 1);
+        /*bloquear boton cuando sobrepasa máximo de carácteres*/
+    var long= msg.value.length;
+         var button= document.getElementById('btn');
+         if (long>140 || long==0) {
+         button.disabled=true;
+         }else{
+        button.disabled=false;
+      }
+
+      };
+})();	
+
+      
